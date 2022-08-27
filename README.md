fd148ff896071b9eb1208fd920853392b2a0e4e3
Daniel Zegerson <danizeger283@gmail.com>
2022-08-27 18:52:31 +0300

    a little bit css

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 ## To do:
 
 - Take care of the sponsers card. FIND A WAY TO COLVE IT!!!
+
+# FONTS:
+
+- TITLE:
+
+* font-family: 'Pattaya';
+* font-family: 'Henny Penny';
+
+- TEXT
+
+* font-family: 'Roboto', sans-serif;
+
+# COLORS:
+
+- #9453a6 (perpul)
+- #8cbf3f (green)
+- #f2cc0f (yellow)
+- #f2f2f2 (gray)
+- #0d0d0d (black)
--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,20 @@
+// Navbar apear when scroll up - start
+{
+  const nav = document.querySelector("#topmenu");
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      nav.classList.add("nav--hidden");
+    } else {
+      nav.classList.remove("nav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+  });
+}
+// Navbar apear when scroll up - end
+
 //  ############## Search JS start here ##############
 
 const f = document.getElementById("search-form");
@@ -6,7 +23,7 @@ const google = "https://www.google.com/search?q=";
 
 function submitted(event) {
   event.preventDefault();
-  const url = google + "mardi grass" + "+" + q.value;
+  const url = google + "mardi gras" + "+" + q.value;
   const win = window.open(url, "_blank");
   win.focus();
 }
